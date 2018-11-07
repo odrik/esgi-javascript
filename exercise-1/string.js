9498e1ab71c59526c3f6940bee04949b6a6866be
Audric <odrikm@hotmail.fr>
2018-11-07 15:36:32 +0100

    exo

---
+++ exercise-1/string.js
@@ -0,0 +1,83 @@
+function ucFirst(string) {
+	if (typeof string === "string") {
+    	return string.charAt(0).toUpperCase() + string.slice(1);
+    } else {
+    	return "";
+    }
+}
+
+function capitalize(string) {
+	if (typeof string === "string") {
+		return string.toLowerCase()
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
+    .join(' ');
+    } else {
+    	return "";
+    }
+}
+
+function camelCase(string) {
+	if (typeof string === "string") {
+		return string.toLowerCase()
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join('');
+    } else {
+    	return "";
+    }
+}
+
+function snake_case(string) {
+	if (typeof string === "string") {
+		return string.toLowerCase()
+    .split(' ')
+    .map((word) => word).join('_');
+    } else {
+    	return "";
+    }
+}
+
+function leet(string){
+	var obj = {
+	  A : '4',
+	  E : '3',
+	  I : '1',
+	  O : '0',
+	  U : '(_)',
+	  Y : '7'
+	}
+	if (typeof string === "string") {
+		var arr = string.toUpperCase().split('');
+		for (var i in arr){
+			if (obj[arr[i]] == undefined) {
+				arr[i] = arr[i];
+			} else {
+				arr[i] = obj[arr[i]];
+			}
+		}
+		return arr.join('');
+	} else {
+		return "";
+	}
+
+}
+
+function verlan(string) {
+	if (typeof string === "string") {
+		return string.split("").reverse().join("").split(" ").reverse().join(" ");
+	} else {
+		return "";
+	}
+}
+
+function yoda(string) {
+	if (typeof string === "string") {
+		return string.split("").reverse().join("").split(" ").reverse().join(" ").split(" ").reverse().join(" ");
+	} else {
+		return "";
+	}
+}
+
+//console.log(ucFirst("bonsoir lae kr aunb yere"));
+
+
